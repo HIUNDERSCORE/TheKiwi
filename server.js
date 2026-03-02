5b351ae2e8b3746f5948f29e6de52f6bb94e6a04
HIUNDERSCORE <128417733+HIUNDERSCORE@users.noreply.github.com>
2026-03-01 19:14:31 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname));
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
+app.listen(port, () => console.log(`Server started on port ${port}`));
